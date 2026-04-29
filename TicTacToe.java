4096023d3244f9f3140d4db342fe598dfeb60cf1
si4345 <si4345@srmist.edu.in>
2026-04-29 08:31:08 +0530

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class TicTacToe {
 
     static char[][] board = {
@@ -6,42 +8,38 @@ public class TicTacToe {
         {'-', '-', '-'}
     };
 
-    // UC5: Validate move
-    public static boolean isValidMove(int row, int col) {
-
-        // Check bounds
-        if (row < 0 || row > 2 || col < 0 || col > 2) {
-            return false;
-        }
-
-        // Check if cell is empty
-        if (board[row][col] != '-') {
-            return false;
-        }
-
-        return true;
-    }
-
-    // UC6: Place move on board
-    public static void placeMove(int row, int col, char symbol) {
-
-        if (isValidMove(row, col)) {
-            board[row][col] = symbol;   // update board
-            System.out.println("Move placed");
-        } else {
-            System.out.println("Invalid move");
-        }
-    }
+    static char computerSymbol = 'O';
 
     public static void main(String[] args) {
+        computerMove();
+        printBoard();
+    }
 
-        int row = 1;
-        int col = 1;
-        char player = 'X';
+    // UC7: Computer makes random valid move
+    static void computerMove() {
 
-        placeMove(row, col, player);
+        Random rand = new Random();
+        int row, col;
 
-        // Display board after move
+        while (true) {
+
+            int slot = rand.nextInt(9) + 1; // generate 1–9
+
+            // Convert slot → row & column
+            row = (slot - 1) / 3;
+            col = (slot - 1) % 3;
+
+            // Check if cell is empty
+            if (board[row][col] == '-') {
+                board[row][col] = computerSymbol;
+                System.out.println("Computer chose slot: " + slot);
+                break;
+            }
+        }
+    }
+
+    // Display board
+    static void printBoard() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(board[i][j] + " ");
